4f48d51e25e6bbc473d5f6beb3a6ec61ec234e3d
Eric Leclerc <eric.leclerc@gmail.com>
2010-01-16 21:31:42 -0500

    added - 'watch' link

--- apps/frontend/modules/video/templates/listSuccess.php
+++ apps/frontend/modules/video/templates/listSuccess.php
@@ -1,3 +1,7 @@
+<div class="span-24">
 <h2>All Videos</h2>
 
-<?php include_partial('video/list', array('videos' => $videos)) ?>
+<?php include_partial('video/list', array('videos' => $videos, 'watchLink' => true)) ?>
+</div>
+
+
