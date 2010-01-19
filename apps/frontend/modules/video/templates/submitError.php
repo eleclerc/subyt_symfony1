258792bde2dcc5bc566e1f240a42778b85d87cb1
Eric Leclerc <eric@mediamiser.com>
2010-01-19 11:55:13 -0500

    video submit error page

---
+++ apps/frontend/modules/video/templates/submitError.php
@@ -0,0 +1,8 @@
+<h2>Error with URL</h2>
+
+<div class="span-24">
+    <p>We're sorry, <?php echo $error ?></p>
+    <p><em><?php echo $video['url'] ?></em></p>
+    <p>Please <a href="<?php echo url_for('home/index') ?>">try again</a>
+</div>
+
