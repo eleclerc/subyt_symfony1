a09993629ca72075ce8ee8e8247d6cdb256bf952
Eric Leclerc <eric@mediamiser.com>
2010-01-19 10:02:46 -0500

    dancer's website link

--- apps/frontend/modules/dancer/templates/showSuccess.php
+++ apps/frontend/modules/dancer/templates/showSuccess.php
@@ -1,6 +1,6 @@
 <div class="span-24">
 <h2>Dancer :: <?php echo $dancer['name'] ?></h2>
-<h4><?php echo $dancer['website'] ?></h4>
+<h4><?php echo $dancer['website'] ?> <a href="<?php echo $dancer['website'] ?>"><img border="0" src="/images/external.png" /></a></h4>
 
 <?php include_partial('video/list', array('videos' => $dancer->Videos, 'watchLink' => true)) ?>
 </div>
