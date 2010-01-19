efcf8da52390f8be5d389ec1883df5f4a5617b60
Eric Leclerc <eric@mediamiser.com>
2010-01-19 10:07:37 -0500

    paths for ZendServer CE and Symfony via PEAR

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once realpath(dirname(__FILE__) . '/../../lib/symfony-1.4.x/lib/autoload/sfCoreAutoload.class.php');
+require_once 'symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
@@ -22,7 +22,7 @@ class ProjectConfiguration extends sfProjectConfiguration
  
     //set_include_path(sfConfig::get('sf_lib_dir').'/vendor'.PATH_SEPARATOR.get_include_path());
     //require_once sfConfig::get('sf_lib_dir').'/vendor/Zend/Loader/Autoloader.php';
-    require_once realpath(dirname(__FILE__) . '/../../lib/ZendFramework-1.9.7/library/Zend/Loader/Autoloader.php'); 
+    require_once 'Zend/Loader/Autoloader.php'; 
     Zend_Loader_Autoloader::getInstance();
     self::$zendLoaded = true;
   }
